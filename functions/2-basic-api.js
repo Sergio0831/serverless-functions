c95b12aaf7323bb9b80aa5c089369f8f4cd66aba
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-10-08 21:08:10 +0100

    Cors Error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require("../assets/data");
 // domain/.netlify/functions/1-hello
 exports.handler = async (event, context) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
     statusCode: 200,
     body: JSON.stringify(items)
   };
